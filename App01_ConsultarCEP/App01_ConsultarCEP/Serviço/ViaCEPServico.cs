eb7d9d6511bd4a3c13c3b6b2f18a01b8c25ebd9d
Kemely Cardozo <kemely.cardozo@programmers.com.br>
2019-02-28 16:50:29 -0300

    Initial commit

---
+++ App01_ConsultarCEP/App01_ConsultarCEP/Serviço/ViaCEPServico.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App01_ConsultarCEP.Serviço
+{
+    class ViaCEPServico
+    {
+        private static string EnderecoURL = "http://viacep.com.br/ws/{0}/json/";
+    }
+}
